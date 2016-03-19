71c6d46c0d4d6bf8924ff04518226b7440d3dec4
mngola <mikambingongola@yahoo.com>
2016-03-19 11:15:42 -0400

    first commit

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour {
+
+		public Transform player;
+		bool hasPlayer = false;
+		bool beingCarried = false;
+
+		void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.gameObject.CompareTag ("Player")) 
+			    {
+					hasPlayer = true;
+				}
+		}
+
+		void OnTriggerExit2D(Collider2D other)
+		{
+			hasPlayer = false;
+		}
+
+		void FixedUpdate()
+		{
+			if(beingCarried)
+			{
+			if(Input.GetKeyDown(KeyCode.DownArrow))
+				{
+				transform.SetParent(null);
+					beingCarried = false;
+				}
+			}
+			else
+			{
+				if(hasPlayer && Input.GetKeyDown(KeyCode.DownArrow))
+					{
+					transform.SetParent (player);
+				    beingCarried = true;
+					}
+			}
+		}
+	}
---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Walker : MonoBehaviour {
+	public float walkSpeed = 2.0f;
+	public float moveForce = 3f;
+	public int startHealth = 10;
+	public int currentHealth;
+	public Slider healthSlider;
+
+	private Rigidbody2D rb2d;
+
+
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		currentHealth = startHealth;
+	}
+
+	void FixedUpdate ()
+	{
+		if (rb2d.velocity.x < walkSpeed) {
+			rb2d.AddForce (Vector2.right * moveForce);
+		}
+
+		if (Mathf.Abs (rb2d.velocity.x) > walkSpeed) {
+			rb2d.velocity = new Vector2 (Mathf.Sign(rb2d.velocity.x) * walkSpeed, rb2d.velocity.y);
+		}
+			
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Obstacle")) 
+		{
+			moveForce = 6f;
+		}
+		if (other.gameObject.CompareTag ("Danger")) 
+		{
+			TakeDamage (1);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Obstacle")) 
+		{
+			moveForce = 1f;
+		}
+	}
+
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+		if (currentHealth <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
